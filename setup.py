7f2b0fae3391f1af85e1cf657c6c4d49aed21485
Nicolas Frick <nicolas.frick@hitec-hamburg.de>
2025-10-30 02:17:10 +0100

    migrate build setup to ros2

--- setup.py
+++ setup.py
@@ -1,6 +1,4 @@
-from setuptools import find_packages, setup
-import os
-from glob import glob
+from setuptools import setup, find_packages
 
 package_name = 'camera_pose'
 
@@ -9,22 +7,21 @@ setup(
     version='0.0.0',
     packages=find_packages(exclude=['test']),
     data_files=[
-        ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
+        ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
-        (os.path.join('share', package_name, 'launch'), glob('launch/*.launch.py')),
-        (os.path.join('share', package_name, 'config'), glob('config/*')),
+        ('share/' + package_name + '/launch', ['launch/cam_pose.launch.py', 'launch/realsense.launch.py']),
+        ('share/' + package_name + '/config', []),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
     maintainer='Nicolas Frick',
-    maintainer_email='nicolas.frick@uni-hamburg.de',
-    description='TODO: Package description',
-    license='TODO: License declaration',
+    maintainer_email='nicolas.frick@studium.uni-hamburg.de',
+    description='ROS 2 port of camera pose estimation using AprilTags.',
+    license='MIT',
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            f'some_service = {package_name}.some_service:main'
+            'camera_pose_node = camera_pose.detect_node:main',
         ],
     },
 )
